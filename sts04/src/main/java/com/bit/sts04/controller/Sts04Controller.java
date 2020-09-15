257f22fd34936aecc81c9acb33b5e2231f0686f1
Moonybb <moony_bb@naver.com>
2020-09-15 17:56:24 +0900

    Spring 3.x - ibatis&mybatis

---
+++ sts04/src/main/java/com/bit/sts04/controller/Sts04Controller.java
@@ -0,0 +1,30 @@
+package com.bit.sts04.controller;
+
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Sts04Controller {
+	@Autowired
+	DataSource dataSource;
+	
+	@RequestMapping("/")
+	public String index(Model model) throws SQLException {
+		String[] arr = {
+				"big01.jpg",
+				"big02.jpg",
+				"big03.jpg",
+				"big04.jpg",
+				"big05.jpg",
+				"big06.jpg"
+		};
+		model.addAttribute("imgs", arr);
+		return "home";
+	}
+}
---
+++ sts04/src/main/java/com/bit/sts04/model/EmpDaoIbatisImpl.java
@@ -0,0 +1,42 @@
+package com.bit.sts04.model;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+
+import com.bit.sts04.model.entity.EmpVo;
+
+public class EmpDaoIbatisImpl implements EmpDao {
+//	@Autowired
+//	SqlMapClient sqlMapClient;
+	
+	@Override
+	public List<EmpVo> selectAll() throws SQLException {
+		return null;
+//		return sqlMapClient.queryForList("selectAll");
+	}
+
+	@Override
+	public EmpVo selectOne(int key) throws SQLException {
+		return null;
+	}
+
+	@Override
+	public void insertOne(EmpVo bean) throws SQLException {
+//		sqlMapClient.insert("insertOne",bean);
+	}
+
+	@Override
+	public int updateOne(EmpVo bean) throws SQLException {
+		return 0;
+	}
+
+	@Override
+	public int deleteOne(int key) throws SQLException {
+		return 0;
+	}
+
+}
