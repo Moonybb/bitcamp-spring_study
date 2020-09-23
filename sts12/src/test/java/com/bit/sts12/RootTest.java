db538880ef5f343d7d3ae6610128a58518d6e400
Moonybb <moony_bb@naver.com>
2020-09-23 18:09:45 +0900

    Spring java configure

---
+++ sts12/src/test/java/com/bit/sts12/RootTest.java
@@ -0,0 +1,44 @@
+package com.bit.sts12;
+
+import static org.junit.Assert.assertNotNull;
+
+import java.sql.SQLException;
+
+import javax.inject.Inject;
+import javax.sql.DataSource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.bit.sts12.config.RootConfig;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = RootConfig.class)
+public class RootTest {
+	@Inject
+	DataSource dataSource;
+	
+	@Test
+	public void test() throws SQLException{
+		assertNotNull(dataSource.getConnection());
+	}
+	
+	
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
